e38a0a012c4e0d826ef0ad85c17bffb19d45dfa2
GianfrancoMS <gianfrancomonsal@gmail.com>
2018-01-22 01:39:47 -0500

    Update Kosaraju's algorithm

--- Graph Search Algorithms/Kosaraju's algorithm.cpp
+++ Graph Search Algorithms/Kosaraju's algorithm.cpp
@@ -12,94 +12,102 @@ public:
 
     ~AdjacencyList() = default;
 
-    void insertVertex(int v) {
-        if (list.find(v) == list.end())
-            list[v]=set<int>{};
+    void insertVertex(int vertex) {
+        if (vertices.find(vertex) == vertices.end())
+            vertices[vertex] = set < int > {};
     }
 
     void insertEdge(int u, int v) {
-        if (list.find(u) != list.end())
-            list[u].insert(v);
+        if (vertices.find(u) != vertices.end())
+            vertices[u].insert(v);
         else
-            list[u] = set<int>{ v };
+            vertices[u] = set < int > {v};
         insertVertex(v);
     }
 
     void printList() {
-        cout << endl;
-        for (auto i = list.begin(); i != list.end(); ++i) {
-            cout << i->first << ": ";
-            for (auto j = i->second.begin(); j != i->second.end(); ++j)
-                cout << *j << " ";
+        for (auto vertex: vertices) {
+            cout << vertex.first << ": ";
+            for (auto adjacent: vertex.second)
+                cout << adjacent << " ";
             cout << endl;
         }
     }
 
-    AdjacencyList getTranspose(){
+    AdjacencyList getTranspose() {
         AdjacencyList transpose = AdjacencyList();
-        for(auto i=list.begin();i!=list.end();++i)
-            for(auto j=i->second.begin();j!=i->second.end();++j)
-                transpose.insertEdge(*j,i->first);
+        for (auto vertex: vertices)
+            for (auto adjacent: vertex.second)
+                transpose.insertEdge(adjacent, vertex.first);
         return transpose;
     }
 
-    void printConnectedComponents(){
-        loadNodes();
-        stack<int>Stack;
-        for(auto i=list.begin();i!=list.end();++i)
-            if(!unvisitedNodes[i->first])
-                fillOrder(i->first,Stack);
-        auto transpose=this->getTranspose();
-        loadNodes();
-        while(!Stack.empty()){
-            int v=Stack.top();
-            Stack.pop();
-            if(!unvisitedNodes[v]){
-                transpose.DFSUtil(v);
-                cout<<endl;
+    void printConnectedComponents() {
+        loadUnvisitedVertices();
+
+        stack<int> verticesStack;
+        for (auto vertex: vertices)
+            if (!unvisitedVertices[vertex.first])
+                fillOrder(vertex.first, verticesStack);
+
+        auto transpose = this->getTranspose();
+
+        loadUnvisitedVertices();
+
+        while (!verticesStack.empty()) {
+            int currentVertex = verticesStack.top();
+            verticesStack.pop();
+            if (!unvisitedVertices[currentVertex]) {
+                transpose.DFSUtil(currentVertex);
+                cout << endl;
             }
         }
     }
 
 private:
 
-    map < int, set<int> > list;
-    map < int, bool > unvisitedNodes;
+    map<int, set<int> > vertices;
 
-    void loadNodes(){
-        if(!unvisitedNodes.empty())
-            unvisitedNodes.clear();
-        for(auto i=list.begin();i!=list.end();++i){
-            unvisitedNodes[i->first]=false;
+    map<int, bool> unvisitedVertices;
+
+    void loadUnvisitedVertices() {
+        if (!unvisitedVertices.empty())
+            unvisitedVertices.clear();
+        for (auto vertex: vertices) {
+            unvisitedVertices[vertex.first] = false;
         }
     }
 
-    void fillOrder(int v, stack<int>&Stack){
-        unvisitedNodes[v]= true;
-        for(auto i=list[v].begin();i!=list[v].end();++i)
-            if(!unvisitedNodes[*i])
-                fillOrder(*i,Stack);
-        Stack.push(v);
+    void fillOrder(int vertex, stack<int> &verticesStack) {
+        unvisitedVertices[vertex] = true;
+
+        auto adjacentVertices = vertices[vertex];
+        for (auto adjacent: adjacentVertices)
+            if (!unvisitedVertices[adjacent])
+                fillOrder(adjacent, verticesStack);
+
+        verticesStack.push(vertex);
     }
 
-    void DFSUtil(int v){
-        unvisitedNodes[v]= true;
-        cout<<v<<" ";
-        for(auto i=list[v].begin();i!=list[v].end();++i)
-            if(!unvisitedNodes[*i])
-                DFSUtil(*i);
+    void DFSUtil(int vertex) {
+        unvisitedVertices[vertex] = true;
+        cout << vertex << " ";
+
+        auto adjacentVertices = vertices[vertex];
+        for (auto adjacent: adjacentVertices)
+            if (!unvisitedVertices[adjacent])
+                DFSUtil(adjacent);
     }
+
 };
 
-/*
-int main(){
+int main() {
     AdjacencyList list = AdjacencyList();
     list.insertEdge(1, 2);
     list.insertEdge(1, 10);
     list.insertEdge(1, 3);
     list.insertEdge(2, 10);
-    list.insertEdge(2,1);
-    list.printList();
+    list.insertEdge(2, 1);
     list.printConnectedComponents();
     return 0;
-}*/
+}
