792e2e9150adde5a9dc2e4bd8c4d2eddf82bb776
GianfrancoMS <gianfrancomonsal@gmail.com>
2018-01-22 23:59:58 -0500

    Update topological sort's algorithm

--- Graph Search Algorithms/Topological Sort.cpp
+++ Graph Search Algorithms/Topological Sort.cpp
@@ -12,72 +12,78 @@ public:
 
     ~AdjacencyList() = default;
 
-    void insertVertex(int v) {
-        if (list.find(v) == list.end())
-            list[v]=set<int>{};
+    void insertVertex(int vertex) {
+        if (vertices.find(vertex) == vertices.end())
+            vertices[vertex] = set < int > {};
     }
 
     void insertEdge(int u, int v) {
-        if (list.find(u) != list.end())
-            list[u].insert(v);
+        if (vertices.find(u) != vertices.end())
+            vertices[u].insert(v);
         else
-            list[u] = set<int>{ v };
+            vertices[u] = set < int > {v};
         insertVertex(v);
     }
 
     void printList() {
-        cout << endl;
-        for (auto i = list.begin(); i != list.end(); ++i) {
-            cout << i->first << ": ";
-            for (auto j = i->second.begin(); j != i->second.end(); ++j)
-                cout << *j << " ";
+        for (auto vertex: vertices) {
+            cout << vertex.first << ": ";
+            for (auto adjacent: vertex.second)
+                cout << adjacent << " ";
             cout << endl;
         }
     }
 
-    void topologicalSort(){
-        loadNodes();
-        stack<int>Stack;
-        for(auto i=list.begin();i!=list.end();++i)
-            if(!unvisitedNodes[i->first])
-                topologicalSortUtil(i->first,Stack);
-        while(!Stack.empty()){
-            cout<<Stack.top()<<" ";
-            Stack.pop();
+    void topologicalSort() {
+        loadUnvisitedVertices();
+
+        stack<int> verticesStack;
+        for (auto vertex: vertices)
+            if (!unvisitedVertices[vertex.first])
+                topologicalSortUtil(vertex.first, verticesStack);
+
+        while (!verticesStack.empty()) {
+            cout << verticesStack.top() << " ";
+            verticesStack.pop();
         }
     }
 
 private:
-    map < int, set<int> > list;
-    map < int, bool > unvisitedNodes;
 
-    void loadNodes(){
-        if(!unvisitedNodes.empty())
-            unvisitedNodes.clear();
-        for(auto i=list.begin();i!=list.end();++i){
-            unvisitedNodes[i->first]=false;
+    map<int, set<int> > vertices;
+
+    map<int, bool> unvisitedVertices;
+
+    void loadUnvisitedVertices() {
+        if (!unvisitedVertices.empty())
+            unvisitedVertices.clear();
+        for (auto vertex: vertices) {
+            unvisitedVertices[vertex.first] = false;
         }
     }
 
-    void topologicalSortUtil(int v, stack<int>&Stack){
-        unvisitedNodes[v]=true;
-        for(auto i=list[v].begin();i!=list[v].end();++i)
-            if(!unvisitedNodes[*i])
-                topologicalSortUtil(*i,Stack);
-        Stack.push(v);
+    void topologicalSortUtil(int vertex, stack<int> &verticesStack) {
+        unvisitedVertices[vertex] = true;
+
+        auto adjacentVertices = vertices[vertex];
+        for (auto adjacent: adjacentVertices)
+            if (!unvisitedVertices[adjacent])
+                topologicalSortUtil(adjacent, verticesStack);
+
+        verticesStack.push(vertex);
     }
+
 };
 
-/*
-int main(){
+int main() {
     AdjacencyList list = AdjacencyList();
     list.insertEdge(1, 2);
     list.insertEdge(1, 10);
     list.insertEdge(1, 3);
     list.insertEdge(2, 10);
-    list.insertEdge(2,1);
+    list.insertEdge(2, 1);
     list.printList();
     list.topologicalSort();
     return 0;
-}*/
+}
 
